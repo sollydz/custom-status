5954221348af4bb663293ed646f2275f9cd7b04f
sollydz <157352727+sollydz@users.noreply.github.com>
2024-04-23 11:46:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { launcher } = require("@loybung/launcher");
 const path = require("path");
 
+const keep = require("./keep_alive");
 const options = {
   url: "https://loybung.vercel.app/api/project/customstatus",
   filepath: path.resolve(__dirname, "app.js"),
